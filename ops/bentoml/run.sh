72aa8cf72a3849fbe3ae3012b6439b2c293038c0
Fidel R <fidelramadhan@gmail.com>
2024-08-19 01:26:10 +0700

    chore: modularize architecture and update run.sh

---
+++ ops/bentoml/run.sh
@@ -0,0 +1,5 @@
+# VLLM_VERSION="0.5.4"
+# pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/nightly/vllm-${VLLM_VERSION}-cp38-abi3-manylinux1_x86_64.whl
+
+# VLLM_VERSION=$VLLM_VERSION pip install -r requirements.txt
+pip install -r requirements.txt --force-reinstall --no-cache-dir
--- run.sh
+++ run.sh
@@ -1,5 +1,29 @@
-# VLLM_VERSION="0.5.4"
-# pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/nightly/vllm-${VLLM_VERSION}-cp38-abi3-manylinux1_x86_64.whl
+!/bin/bash
 
-# VLLM_VERSION=$VLLM_VERSION pip install -r requirements.txt
-pip install -r requirements.txt --force-reinstall --no-cache-dir
+Define the path to the base directory containing the requirements files
+
+BASE_DIR="${PWD}"
+
+Determine which requirements file to use based on the user command
+
+USER_COMMAND=$1
+BASE_ENGINE=$2
+
+if [ $BASE_ENGINE == "vllm" ]; then
+    ENGINE_REQUIREMENTS_FILE="${BASE_DIR}/engines/vllm_based/requirements-vllm.txt"
+else
+    echo "Error: Non-empty engine user command. Please use 'vllm' or '..' that supported."
+    exit 1
+fi
+
+if [ $USER_COMMAND == "bentoml" ]; then
+    REQUIREMENTS_FILE="${BASE_DIR}/ops/bentoml/requirements-bentoml.txt"
+elif [ $USER_COMMAND == "ray" ]; then
+    REQUIREMENTS_FILE="${BASE_DIR}/ops/ray/requirements-ray.txt"
+else
+    echo "Error: Invalid user command. Please use 'bentoml' or 'ray'."
+    exit 1
+fi
+
+pip install -r "${ENGINE_REQUIREMENTS_FILE}"
+pip install -r "${REQUIREMENTS_FILE}"
