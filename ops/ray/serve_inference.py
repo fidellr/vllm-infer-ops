957a8ced8b5b605b95b53676e6874b8b0bc90e1d
Fidel R <fidelramadhan@gmail.com>
2024-08-21 05:30:31 +0700

    fix: ray serve client funcs

--- ops/ray/serve_inference.py
+++ ops/ray/serve_inference.py
@@ -1,36 +1,42 @@
-from openai import OpenAI
-from typing import List, Optional
-from vllm import AsyncEngineArgs, AsyncLLMEngine
-from vllm.entrypoints.openai.serving_engine import LoRAModulePath
+import asyncio
+import os
+from typing import Optional
+
+from openai import AsyncOpenAI, OpenAI
+
+# from vllm import AsyncEngineArgs, AsyncLLMEngine
+
+# from vllm.entrypoints.openai.serving_engine import LoRAModulePath
 
 
 class InferenceServe:
     def __init__(
         self,
-        engine_args: AsyncEngineArgs,
+        # engine_args: AsyncEngineArgs,
         response_role: str,
-        lora_modules: Optional[List[LoRAModulePath]] = None,
+        base_url: Optional[str] = None,
+        api_key: Optional[str] = "sk-xxxx",
+        base_model: Optional[str] = "thesven/microsoft_wizardlm-2-7b-gptq",
+        openai_serving_client: Optional[OpenAI | AsyncOpenAI] = None,
         chat_template: Optional[str] = None,
     ) -> None:
-        self.openai_serving_chat = None
-        self.engine_args = engine_args
-        self.response_role = response_role
-        self.lora_modules = lora_modules
-        self.chat_template = chat_template
-        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
+        self.base_url = base_url or os.getenv("OPENAI_BASE_URL")
+        self.api_key = api_key or os.getenv("OPENAI_API_KEY")
+        self.base_model = base_model or os.getenv("OPENAI_BASE_MODEL")
 
-    def chat_completions(self, messages):
-        # Note: Ray Serve doesn't support all OpenAI client arguments and may ignore some.
-        client = OpenAI(
-            # Replace the URL if deploying your app remotely
-            # (e.g., on Anyscale or KubeRay).
-            base_url="http://localhost:8000/v1",
-            api_key="NOT A REAL KEY",
+        self.openai_serving_chat = openai_serving_client or AsyncOpenAI(
+            base_url=self.base_url,
+            api_key=self.api_key,
         )
-        chat_completion = client.chat.completions.create(
+        self.response_role = response_role
+        self.chat_template = chat_template
+
+    async def chat_completions(self):
+        # Note: Ray Serve doesn't support all OpenAI client arguments and may ignore some.
+        chat_completion = await self.openai_serving_chat.chat.completions.create(
             model="NousResearch/Meta-Llama-3-8B-Instruct",
-            messages=messages
-            or [
+            # messages=messages
+            messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {
                     "role": "user",
@@ -40,10 +46,12 @@ class InferenceServe:
             temperature=0.01,
             stream=True,
         )
+
         for chat in chat_completion:
             if chat.choices[0].delta.content is not None:
                 print(chat.choices[0].delta.content, end="")
 
 
 if __name__ == "__main__":
-    inference_engine = InferenceServe()
+    inference_engine = InferenceServe(response_role="assistant")
+    asyncio.run(inference_engine.chat_completions())
