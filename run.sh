20ff0321c224705bd106e22ffcd8b13ba4fb08db
Fidel R <fidelramadhan@gmail.com>
2024-08-21 05:40:14 +0000

    fix: adjust run.sh and dynamic vllm wheels installation

--- run.sh
+++ run.sh
@@ -2,26 +2,27 @@
 
 # Define the path to the base directory containing the requirements files
 BASE_DIR="${PWD}"
+VLLM_VERSION=0.5.4
 
 # Determine which requirements file to use based on the user command
-USER_COMMAND=$1
+MLOPS=$1
 BASE_ENGINE=$2
 
-if [ "$BASE_ENGINE" = "vllm" ]; then
-	ENGINE_REQUIREMENTS_FILE="${BASE_DIR}/engines/vllm_based/requirements-vllm.txt"
-else
-	echo "Error: Non-empty engine user command. Please use 'vllm' or '..' that supported."
-	exit 1
-fi
-
-if [ "$USER_COMMAND" = "bentoml" ]; then
-	REQUIREMENTS_FILE="${BASE_DIR}/ops/bentoml/requirements-bentoml.txt"
-elif [ "$USER_COMMAND" = "ray" ]; then
-	REQUIREMENTS_FILE="${BASE_DIR}/ops/ray/requirements-ray.txt"
+if [ "$MLOPS" = "bentoml" ] || [ "$MLOPS" = "ray" ]; then
+	REQUIREMENTS_FILE="${BASE_DIR}/ops/${MLOPS}/requirements-${MLOPS}.txt"
 else
 	echo "Error: Invalid user command. Please use 'bentoml' or 'ray'."
 	exit 1
 fi
 
-pip install -r "${ENGINE_REQUIREMENTS_FILE}" -v
-pip install -r "${REQUIREMENTS_FILE}" -v
+if [[ ${BASE_ENGINE} == "vllm" ]]; then
+	ENGINE_REQUIREMENTS_FILE="${BASE_DIR}/engines/vllm_based/requirements-vllm.txt"
+	echo "vllm --index-url https://vllm-wheels.s3.us-west-2.amazonaws.com/nightly/vllm-${VLLM_VERSION}-cp38-abi3-manylinux1_x86_64.whl" >"${ENGINE_REQUIREMENTS_FILE}"
+
+else
+	echo "Error: Non-empty engine user command. Please use 'vllm' or '..' that supported."
+	exit 1
+fi
+
+pip install -r "${ENGINE_REQUIREMENTS_FILE}"
+pip install -r "${REQUIREMENTS_FILE}" --force-reinstall
